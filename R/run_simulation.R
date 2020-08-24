aac7ff02d96b3aab0f5b8f562a22bf0ef085dd52
Maximilian Hesselbarth <mhk.hesselbarth@gmail.com>
2020-08-24 12:03:48 -0400

    :white_check_mark: Checks running for first time

---
+++ R/run_simulation.R
@@ -0,0 +1,29 @@
+#' run_simulation
+#'
+#' @description Wrapper functions to run model
+#'
+#' @param parameters List with all parameters.
+#' @param max_i Maximum numbers of ticks to run.
+#' @param verbose If TRUE, prints progress report.
+#'
+#' @details
+#' Wrapper function to run model. Executes the following sub-processes (i) ...
+#' (ii) ...
+#'
+#' Parameters include ...
+#'
+#' @return data frame
+#'
+#' @examples
+#' \dontrun{
+#' parameters <- read.csv(file = "inst/parameters_example.csv")
+#' result <- run_simulation(parameters = parameters, max_i = 10)
+#' }
+#'
+#' @aliases run_simulation
+#' @rdname run_simulation
+#'
+#' @export
+run_simulation <- function(parameters, max_i, verbose = FALSE) {
+
+}
