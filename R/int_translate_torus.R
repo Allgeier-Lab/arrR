4335ead7afb4cae6b62b1e49882dfcc0dc358bfe
Maximilian Hesselbarth <mhk.hesselbarth@gmail.com>
2020-09-22 15:05:26 -0400

    Improve torus

--- R/int_translate_torus.R
+++ R/int_translate_torus.R
@@ -18,18 +18,24 @@
 #' @export
 int_translate_torus <- function(coords, extent) {
 
-  # torus edge correction at boundaries
-  coords[which(coords[, 1] < extent[1]), 1] <- extent[2] -
-    (extent[1] - coords[which(coords[, 1] < extent[1]), 1])
+  # loop in case torus translation is largeer than plot again
+  while (any(c(coords[, 1] < extent[1], coords[, 1] > extent[2],
+               coords[, 2] < extent[3], coords[, 2] > extent[4]))) {
 
-  coords[which(coords[, 1] > extent[2]), 1] <- extent[1] +
-    (coords[which(coords[, 1] > extent[2]), 1] - extent[2])
+    # torus edge correction at boundaries
+    coords[which(coords[, 1] < extent[1]), 1] <- extent[2] -
+      (extent[1] - coords[which(coords[, 1] < extent[1]), 1])
 
-  coords[which(coords[, 2] < extent[3]), 2] <- extent[4] -
-    (extent[3] - coords[which(coords[, 2] < extent[3]), 2])
+    coords[which(coords[, 1] > extent[2]), 1] <- extent[1] +
+      (coords[which(coords[, 1] > extent[2]), 1] - extent[2])
 
-  coords[which(coords[, 2] > extent[4]), 2] <- extent[3] +
-    (coords[which(coords[, 2] > extent[4]), 2] - extent[4])
+    coords[which(coords[, 2] < extent[3]), 2] <- extent[4] -
+      (extent[3] - coords[which(coords[, 2] < extent[3]), 2])
+
+    coords[which(coords[, 2] > extent[4]), 2] <- extent[3] +
+      (coords[which(coords[, 2] > extent[4]), 2] - extent[4])
+
+  }
 
   return(coords)
 }
