099d6416cbe43dffe74761ceef18cc3ef0b709bd
mhesselbarth <mhk.hesselbarth@gmail.com>
2021-08-17 15:46:07 -0400

    :pencil: :globe_with_meridians: Update homepage and vignettes

--- R/arrR-packages.R
+++ R/arrR-packages.R
@@ -4,10 +4,11 @@
 #' Individual-based simulation model to analyze how artificial reefs in combination
 #' with a fish population influence seagrass growth.
 #'
-#' For more information about how to use the model, please see \code{browseVignettes("arrR")}.
+#' For more information about how to use the model, please see \url{https://allgeier-lab.github.io/arrR/}.
 #'
-#' For a detailed model description, see Esquivel, K., Hesselbarth, M.H.K., Allgeier, J.E.
-#' In preparation. Mechanistic support for increased primary production around artificial reefs.
+#' For a detailed model description, see Esquivel, K.E., Hesselbarth, M.H.K., Allgeier, J.E.
+#' Mechanistic support for increased primary production around artificial reefs. Manuscript
+#' submitted for publication.
 #'
 #' @name arrR
 #' @docType package
