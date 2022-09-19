96bb4df6e53db775213908407e9036d3914e2f2b
Katrina Munsterman <kmunster@umich.edu>
2022-09-19 13:33:13 -0400

    adding in fixed mortality term based on pop_mean_size

--- src/rcpp_mortality.cpp
+++ src/rcpp_mortality.cpp
@@ -2,7 +2,6 @@
 
 #include "rcpp_mortality.h"
 #include "rcpp_shuffle.h"
-#include "rcpp_runif.h"
 #include "rcpp_reincarnate.h"
 
 using namespace Rcpp;
@@ -16,13 +15,13 @@ using namespace Rcpp;
 //'
 //' @param fishpop,fishpop_track Matrix with fishpop and starting fishpop values.
 //' @param seafloor Matrix with seafloor values.
-//' @param pop_linf,pop_n_body,pop_reserves_max Numeric with parameters.
+//' @param pop_mean_size,pop_n_body,pop_reserves_max Numeric with parameters.
 //' @param extent Vector with extent (xmin,xmax,ymin,ymax).
 //' @param dimensions Vector with dimensions (nrow, ncol).
 //'
 //' @details
-//' Function to simulate background mortality of fish individuals. The mortality
-//' probability increases with increasing size and approximates p=1 for \code{pop_linf}.
+//' Function to simulate background mortality of fish individuals.
+//' Fish automatically dies when \code{pop_mean_size} + 2.
 //' If a individual dies, a new individual is created.
 //'
 //' @return void
@@ -34,7 +33,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 void rcpp_mortality(Rcpp::NumericMatrix fishpop, Rcpp::NumericMatrix fishpop_track,
                     Rcpp::NumericMatrix seafloor,
-                    double pop_linf, double pop_n_body, double pop_reserves_max,
+                    double pop_mean_size, double pop_n_body, double pop_reserves_max,
                     Rcpp::NumericVector extent, Rcpp::IntegerVector dimensions) {
 
   // create random order if fish id because detritus can run out
@@ -46,18 +45,12 @@ void rcpp_mortality(Rcpp::NumericMatrix fishpop, Rcpp::NumericMatrix fishpop_tra
     // use Rcpp indexing counter of current loop iteration
     int row_id_temp = row_id[i] - 1;
 
-    // create death probability
-    double death_prob = std::exp(fishpop(row_id_temp, 5) - pop_linf);
-
-    // create random number to test death prob against
-    double random_prob = rcpp_runif(0.0, 1.0);
-
-    // individual dies if random number is smaller than death probability
-    if (random_prob < death_prob) {
+    // individual dies current size is larger than pop_mean_size + 2
+    if ((fishpop(row_id_temp, 5) > pop_mean_size + 2)) {
 
       rcpp_reincarnate(fishpop, fishpop_track, row_id_temp,
                        seafloor, extent, dimensions,
-                       pop_linf, pop_n_body, pop_reserves_max,
+                       pop_mean_size, pop_n_body, pop_reserves_max,
                        "background");
 
     }
