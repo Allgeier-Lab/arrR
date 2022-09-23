578cdac4396278735173f214dbc3bbe184ee9f69
Katrina Munsterman <kmunster@umich.edu>
2022-09-23 11:34:05 -0400

    :bomb: updating pop_ldie param

--- src/rcpp_fishpop_growth.h
+++ src/rcpp_fishpop_growth.h
@@ -4,7 +4,7 @@
 void rcpp_fishpop_growth(Rcpp::NumericMatrix fishpop, Rcpp::NumericMatrix fishpop_track,
                          Rcpp::NumericMatrix seafloor,
                          double pop_k, double pop_linf, double pop_a, double pop_b,
-                         double pop_n_body, double pop_reserves_max, double pop_reserves_consumption,
+                         double pop_n_body, double pop_reserves_max, double pop_reserves_consump,
                          Rcpp::NumericVector extent, Rcpp::IntegerVector dimensions,
                          double min_per_i);
 
